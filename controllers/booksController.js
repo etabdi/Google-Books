9aab502098b09801b3fd7b4720ac2ea1d74b9a39
abdu <etabdi@yahoo.com>
2019-12-03 20:17:14 -0600

    update controller

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,29 +1,37 @@
-const express = require('express');
-const db = require('../models');
+const db = require("../models");
 
-const router = express.Router();
-
-router.get('/api/books', (req, res) => {
-
-  db.Book.find({})
-    .then(dbBook => res.json(dbBook))
-    .catch(err => res.status(422).json(err))
-
-});
-
-router.post('/api/books', (req, res) => {
-
-  db.Book.create(req.body)
-    .then(dbBook => res.json(dbBook))
-    .catch(err => res.status(422).json(err));
-});
-
-router.delete('/api/books/:id', (req, res) => {
-
-  db.Book.findByIdAndRemove(req.params.id)
-    .then(dbBook => res.json(dbBook))
-    .catch(err => res.status(422).json(err));
-
-});
-
-module.exports = router;
+// Defining methods for the booksController
+module.exports = {
+  findAll: function(req, res) {
+    db.Book
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Book
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Book
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Book
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Book
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
